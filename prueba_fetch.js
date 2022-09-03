91784ade4f2476f917db1fc32ea4d18b32af6544
andres alvarez <andresalvarezsilva6@gmail.comgit config --globla user.name andresgit config --global user.email andresalvarezsilva6@gmail.com>
2022-09-03 03:32:46 -0400

    late night upgrade

--- prueba_fetch.js
+++ prueba_fetch.js
@@ -12,24 +12,32 @@ function set(){
            let formas = data.abilities.map(form => form.ability.name);
            var movi = data.moves.map(moves => moves.move.name);
            element.innerHTML = `
+          
+    
 
-
- <div class="header">
-  <div class="budy"><p>nombre del pokemon :${data.name} </p> 
+    <div class = "a1">
+    <div class = "bsprite">
+    <button>sprites</button>
+    </div>
+    <div class = "a2">       
+    <div class = "budy">
+    <div class = "a3">
+    <p class="budy> Nombre del pokemon :${data.name}</p> 
     <p class="budy">Numero de pokedex :${data.order}</p> 
     <p class="budy">tipos :${tipos}</p>
     <p class="budy">habilidades :${formas}</p>  
     <p >sprites :</p>  
-     </div>
-      </div>
-    <aside class ="ded">  
-    <div> <img class = "sprite" src="${data.sprites.front_default}"></div>
-    <div><img class = "sprite" src="${data.sprites.back_default}"></div>
-    </aside>
+    </div>
+    </div>
+    </div>
     
+    <div> 
+    <img class = "sprite" src="${data.sprites.front_default}">
+    <img class = "sprite" src="${data.sprites.back_default}">
+    </div>
+    </div>
     
-    
-
+   
     `;
         
            let x = document.querySelector('#sprite');
@@ -44,4 +52,7 @@ function set(){
    .catch(error=> console.log("ERROR"))
    }
 
+   function as(){
+    alert("hola")
+   }
   
