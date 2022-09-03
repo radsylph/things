33d3ed8c5420fc647f6f64485860667c3f644925
andres alvarez <andresalvarezsilva6@gmail.comgit config --globla user.name andresgit config --global user.email andresalvarezsilva6@gmail.com>
2022-09-03 14:35:48 -0400

    some retweaks

--- pokedex.js
+++ pokedex.js
@@ -17,37 +17,24 @@ function set(){
 
     <div class = "a1">
     <div class = "bsprite">
-    <button>sprites</button>
     </div>
     <div class = "a2">       
     <div class = "budy">
     <div class = "a3">
     <p class="budy> Nombre del pokemon :${data.name}</p> 
     <p class="budy">Numero de pokedex :${data.order}</p> 
-    <p class="budy">tipos :${tipos}</p>
-    <p class="budy">habilidades :${formas}</p>  
-    <p >sprites :</p>  
+    <p class="budy">Tipo :${tipos}</p>
+    <p class="budy">Habilidades :${formas}</p>    
     </div>
     </div>
     </div>
-    
-    <div> 
+
+    <div class ="budy"> 
     <img class = "sprite" src="${data.sprites.front_default}">
     <img class = "sprite" src="${data.sprites.back_default}">
     </div>
     </div>
-    
-   
     `;
-        
-           let x = document.querySelector('#sprite');
-           x.innerHTML = `
-           
-          
-          
-           `; 
-        
-        
    })
    .catch(error=> console.log("ERROR"))
    }
@@ -55,4 +42,6 @@ function set(){
    function as(){
     alert("hola")
    }
-  
+ 
+
+    
