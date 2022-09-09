7c1f2d6cbc4361ede6bd3ce1725ee85eb3c1d2b1
andres <andresalvarezsilva6@gmail.com>
2022-09-09 13:42:58 -0400

    rework on the javascript and css features

--- colores.js
+++ colores.js
@@ -1,3 +1,38 @@
 function a(){
-    var obj = document.getElementById("container").style.color="red";
-}
+    var obj = document.getElementById("container");
+    obj.style["color"] = "red";
+    }
+
+    function b(){
+    var obj = document.getElementById("container");
+    obj.style["color"] = "black";
+    }
+
+    function d(){
+        var blue = document.getElementById("i3"); 
+        var red = document.getElementById("i1");
+        var green = document.getElementById("i2");
+        rgb ="rgb("+Number(red.value)+","+Number(green.value)+","+Number(blue.value)+")"
+        function c(){
+        var obj = document.getElementById("color");
+        obj.style["background-color"] = rgb;
+        }
+        return c;
+    }
+
+    function e(){
+        d();
+        var obj = document.getElementById("container");
+        obj.style["color"] = rgb;
+    }
+
+    function f(){
+        d();
+        var obj = document.getElementById("prueba");
+        obj.style["background-color"] = rgb;
+    }
+
+    function g(){
+        var obj = document.getElementById("prueba");
+        obj.style["background-color"] = "white";
+    }
