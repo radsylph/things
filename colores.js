e7b8018fb0534664854d69bb7f5e2debbae2fb7a
andres <andresalvarezsilva6@gmail.com>
2022-09-23 17:44:30 -0400

    minor cosmetic tweaks and enhancements

--- colores.js
+++ colores.js
@@ -12,7 +12,7 @@ function a(){
         var blue = document.getElementById("i3"); 
         var red = document.getElementById("i1");
         var green = document.getElementById("i2");
-        rgb ="rgb("+Number(red.value)+","+Number(green.value)+","+Number(blue.value)+")"
+        var rgb ="rgb("+Number(red.value)+","+Number(green.value)+","+Number(blue.value)+")"
         function c(){
         var obj = document.getElementById("color");
         obj.style["background-color"] = rgb;
@@ -35,4 +35,16 @@ function a(){
     function g(){
         var obj = document.getElementById("prueba");
         obj.style["background-color"] = "white";
+    }
+
+    function random(){
+        var obj = document.getElementById("container");
+        rgb ="rgb("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+")"
+        return rgb;
+    }
+
+    function r(){
+        random();
+        var obj = document.getElementById("container");
+        obj.style["color"] = rgb;
     }
