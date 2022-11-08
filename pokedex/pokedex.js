f7df65741c94f5df599ed86c65c637dfa37780b3
andres <andresalvarezsilva6@gmail.com>
2022-11-08 08:22:20 -0400

    .

--- pokedex/pokedex.js
+++ pokedex/pokedex.js
@@ -1,35 +1,34 @@
+async function set() {
+  x = document.getElementById("nombre").value;
 
-async function set(){
-    x = document.getElementById("nombre").value;
+  const url2 = "https://pokeapi.co/api/v2/pokemon/";
+  const url3 = "https://pokeapi.co/api/v2/pokemon-species/";
+  var url = url2 + x.toLowerCase();
+  var url1 = url3 + x.toLowerCase();
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Api loaded");
+      console.log(data);
+      fetch(url1)
+        .then((res) => res.json())
+        .then((data) => {
+          var descrip = data.flavor_text_entries;
+          descrip = data.flavor_text_entries[9]["flavor_text"];
+          console.log(data);
+          console.log(descrip);
+          y = document.querySelector("#prueba");
+          y.innerHTML = descrip;
+        });
 
-  const url2 ="https://pokeapi.co/api/v2/pokemon/";
-  const url3= "https://pokeapi.co/api/v2/pokemon-species/"
-    var url = url2 + x.toLowerCase();
-    var url1 = url3 + x.toLowerCase();
-    fetch(url)
-   .then(response => response.json())
-   .then(data => {
-        console.log("Api loaded");
-        console.log(data)
-        fetch(url1)
-        .then(res => res.json())
-        .then(data => {
-         var descrip = data.flavor_text_entries;
-         descrip=data.flavor_text_entries[9]["flavor_text"];
-         console.log(data);
-         console.log(descrip);
-         y = document.querySelector("#prueba");
-         y.innerHTML = descrip;
-        })
-        
-          var descrip = data.species.url;
-           var element = document.querySelector("#pokemon");
-           let tipos = data.types.map(tipe => tipe.type.name);
-           let formas = data.abilities.map(form => form.ability.name);
-           let nombre = data.name;
-           let orden = data.order;
-           console.log(descrip);
-           element.innerHTML = `
+      var descrip = data.species.url;
+      var element = document.querySelector("#pokemon");
+      let tipos = data.types.map((tipe) => tipe.type.name);
+      let formas = data.abilities.map((form) => form.ability.name);
+      let nombre = data.name;
+      let orden = data.order;
+      console.log(descrip);
+      element.innerHTML = `
 
     <div class = "a1">
     <div class = "a2">       
@@ -57,32 +56,28 @@ async function set(){
     </div>
 
     `;
-    
+    })
+    .catch((error) => ds());
+}
+function ds() {
+  setTimeout(() => {
+    as();
+    setTimeout(() => {
+      cd();
+    }, "2000");
+  }, "10");
+}
 
-   })
-   .catch(error=> ds())
-   }
-   function ds(){
-    setTimeout(() => 
-    {
-      as()
-      setTimeout(() => {
-        cd()
-      }, "2000");
-    } , "10")
-   }
+function as() {
+  //prueba para que la pokebola solo se mueva cuando halla un error en el nombre del pokemon
+  let obj = document.querySelector(".pokeball");
+  obj.style.animation = "shake 2s ";
+}
 
-   function as(){ 
-    //prueba para que la pokebola solo se mueva cuando halla un error en el nombre del pokemon
-    let obj = document.querySelector(".pokeball");
-    obj.style.animation = 'shake 2s ';
-   }
+function cd() {
+  let obj = document.querySelector(".pokeball");
 
-   function cd(){
-    let obj = document.querySelector(".pokeball");
+  obj.style.animation = "nose 2s";
+}
 
-    obj.style.animation = 'nose 2s';
-   }
-   
- //https://www.youtube.com/watch?v=dVtnFH4m_fE
-
+//https://www.youtube.com/watch?v=dVtnFH4m_fE
