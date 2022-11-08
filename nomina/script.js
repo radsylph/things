3d0d5e73781724509358e019692170b645712fec
andres <andresalvarezsilva6@gmail.com>
2022-11-07 22:18:08 -0400

    0.0.1

---
+++ nomina/script.js
@@ -0,0 +1,26 @@
+var Concepto = {
+    id_Concepto : "",
+    monto : "",
+    decripcion : "",
+    tipo : ""
+}
+
+var Empelado = {
+    nombre_empleado : "",
+    apellido_empleado : "",
+    cedula_empleado :"",
+    cargo_empelado :"",
+    Concepto : []
+}
+
+var cargo = [
+    {descripcion : "programador junior" , salario : 500 },
+    {descripcion : "programador senior" , salario : 1000},
+    {descripcion : "jefe de proyector" , salario : 1500},
+    {descriptcion : "gerente de proyectos" , salario : 2000}, 
+]
+
+function show(){
+ 
+    
+}
